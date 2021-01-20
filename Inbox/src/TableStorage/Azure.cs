6720bf7bf0d624824c7db5d88836779d8d029105
Jonas Hvid <mail@johv.dk>
2021-01-20 23:05:44 +0100

    Implement TableStorage for Azure

---
+++ Inbox/src/TableStorage/Azure.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Azure.Cosmos.Table;
+
+namespace Inbox.TableStorage {
+    public class AzureTable : ITable {
+        private readonly CloudTable cloudTable;
+
+        public AzureTable(CloudTable cloudTable) {
+            this.cloudTable = cloudTable;
+        }
+
+        public void Insert(IEntity entity) {
+            var properties = new Dictionary<string, EntityProperty>();
+            foreach (var (key, value) in entity.Properties) {
+                properties[key] = new EntityProperty(value);
+            }
+            var azureEntity = new DynamicTableEntity(entity.Partition, entity.Row, "", properties);
+            cloudTable.Execute(TableOperation.Insert(azureEntity));
+        }
+    }
+}
