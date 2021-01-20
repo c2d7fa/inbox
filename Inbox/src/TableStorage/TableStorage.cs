7950db79c8fc702f382c5576d633293a7cc6b594
Jonas Hvid <mail@johv.dk>
2021-01-20 22:53:13 +0100

    Sketch 'IEntity' and 'ITable'

---
+++ Inbox/src/TableStorage/TableStorage.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+
+namespace Inbox.TableStorage {
+    public class Entity : IEntity {
+        public string Partition { get; }
+        public string Row { get; }
+
+        private readonly Dictionary<string, dynamic> properties = new Dictionary<string, dynamic>();
+
+        public Entity(string partition, string row) {
+            Partition = partition;
+            Row = row;
+        }
+
+        public IReadOnlyDictionary<string, dynamic> Properties => properties;
+
+        public void Set(string key, dynamic value) {
+            properties[key] = value;
+        }
+    }
+
+    public interface IEntity {
+        string Partition { get; }
+        string Row { get; }
+        IReadOnlyDictionary<string, dynamic> Properties { get; }
+    }
+
+    public interface ITable {
+        void Insert(IEntity entity);
+    }
+}
