d7740f011d626c90eb0c23427a2d74c9efd5d296
Jonas Hvid <mail@johv.dk>
2021-01-24 15:25:51 +0100

    Show list of unread messages on page

--- Server/Pages/Index.cshtml.cs
+++ Server/Pages/Index.cshtml.cs
@@ -10,10 +10,12 @@ namespace Server.Pages {
         private readonly ITable authentication;
 
         public bool IsAuthenticated => Authentication.IsAuthenticated(HttpContext.Request, authentication);
+        public UnreadMessages UnreadMessages { get; private set; }
 
         public IndexModel(ILogger<IndexModel> log, CloudTableClient client) {
             this.authentication = new AzureTable(client.GetTableReference("Authentication"));
             this.log = log;
+            UnreadMessages = new UnreadMessages(new AzureTable(client.GetTableReference("UnreadMessages")));
         }
 
         public void OnGet() { }
