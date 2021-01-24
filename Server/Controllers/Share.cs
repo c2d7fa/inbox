d7501a72e8b65d4709f61984a126413c83fcf5d8
Jonas Hvid <mail@johv.dk>
2021-01-24 21:52:59 +0100

    Add 'Share' endpoint to new server

---
+++ Server/Controllers/Share.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos.Table;
+using Microsoft.Extensions.Logging.Abstractions;
+using Api = Inbox.Core.Api;
+
+namespace Server.Controllers {
+    [ApiController]
+    [Route("/api/Share")]
+    public class Share : ControllerBase {
+        [HttpPost]
+        public IActionResult Get([FromServices] CloudTableClient client) {
+            var unread = new AzureTable(client.GetTableReference("UnreadMessages"));
+
+            var api = new Api.Share(unread, NullLogger.Instance);
+            return api.Respond(Request);
+        }
+    }
+}
