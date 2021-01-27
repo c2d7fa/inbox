dcbada260326e22f0560df4b5d649ce51f9d3f11
Jonas Hvid <mail@johv.dk>
2021-01-27 19:17:40 +0100

    Remove unused code

--- Server/IStorage.cs
+++ Server/IStorage.cs
@@ -8,26 +8,4 @@ namespace Inbox.Server {
         public void Create(Guid uuid, IPAddress author, string content);
         public void Read(Guid uuid);
     }
-
-    public class MirroredStorage : IStorage {
-        private IStorage primary;
-        private IStorage mirror;
-
-        public MirroredStorage(IStorage primary, IStorage mirror) {
-            this.primary = primary;
-            this.mirror = mirror;
-        }
-
-        public IEnumerable<Message> Unread => primary.Unread;
-
-        public void Create(Guid uuid, IPAddress author, string content) {
-            primary.Create(uuid, author, content);
-            mirror.Create(uuid, author, content);
-        }
-
-        public void Read(Guid uuid) {
-            primary.Read(uuid);
-            mirror.Read(uuid);
-        }
-    }
 }
--- Server/Message.cs
+++ Server/Message.cs
@@ -4,16 +4,16 @@ using System.Web;
 
 namespace Inbox.Server {
     public class Message {
-        public Guid Uuid { get; private set; }
-        public DateTime Created { get; private set; }
-        public IPAddress Author { get; private set; }
-        public string Content { get; private set; }
+        public Guid Uuid { get; }
+        public DateTime Created { get; }
+        public IPAddress Author { get; }
+        public string Content { get; }
 
         public Message(Guid uuid, DateTime created, IPAddress author, string content) {
-            this.Uuid = uuid;
-            this.Created = created;
-            this.Author = author;
-            this.Content = content;
+            Uuid = uuid;
+            Created = created;
+            Author = author;
+            Content = content;
         }
 
         public string HtmlContent {
