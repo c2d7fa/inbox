252443df9496580fb33f4cf49d1a280e7e26b60e
Jonas Hvid <mail@johv.dk>
2021-03-12 01:51:35 +0100

    Return response from service worker when offline
    This will be required to meet installability criteria:
    
    https://developer.chrome.com/blog/improved-pwa-offline-detection/
    
    We just return a custom error message. I don't expect this to actually result in
    any different behavior.

--- Server/wwwroot/service-worker.js
+++ Server/wwwroot/service-worker.js
@@ -1,5 +1,9 @@
 // Having a service worker is necessary to support Share Target API. We don't
 // actually need the worker for anything.
 self.addEventListener("fetch", (event) => {
-    console.log("Service worker: fetch: %o", event);
+    event.respondWith(
+        fetch(event.request)
+            .then(response => response)
+            .catch(err => new Response("Error: Unable to fetch. This is most likely a network error, i.e. you're not connected to the internet or the site is offline. " + JSON.stringify(err)))    
+    );
 });
